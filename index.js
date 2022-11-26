8fea460e0caabe35b4b7907ac69f5c47cb642010
khalid-zein <zeinkhalid004@gmail.com>
2022-11-26 22:07:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = "Khalid Zein"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser}!`.toUpperCase()
+
+const firstInitial = currentUser[0]
+const restOfName = currentUser.slice(1)
+
+const shortGreeting = `Welcome, ${firstInitial}!`
